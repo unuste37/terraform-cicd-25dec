97249f30d8f691d4128a58798bd6cd3f8c2c0673
Your Name <you@example.com>
2025-12-25 14:00:57 +0000

    updated index.html

--- user_data.sh
+++ user_data.sh
@@ -3,5 +3,5 @@ sudo yum update -y
 sudo yum install -y nginx
 sudo systemctl enable nginx
 sudo systemctl start nginx
-echo "<h1>Deployed via Terraform + CodePipeline!</h1>" | sudo tee /usr/share/nginx/html/index.html
+echo "<h1>ReDeployed by Unus!</h1>" | sudo tee /usr/share/nginx/html/index.html
 
